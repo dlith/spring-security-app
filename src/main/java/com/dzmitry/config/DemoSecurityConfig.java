dcc3f2a018a0733a14160265adbb00c7a0b817f4
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-01 14:03:08 +0200

    logout btn support

--- src/main/java/com/dzmitry/config/DemoSecurityConfig.java
+++ src/main/java/com/dzmitry/config/DemoSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -51,7 +52,7 @@ public class DemoSecurityConfig {
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
 				.permitAll())
-		
+                .logout(LogoutConfigurer::permitAll)
 		.build();  
     }	
 }
--- target/spring-security-demo/WEB-INF/view/home.jsp
+++ target/spring-security-demo/WEB-INF/view/home.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 
 <head>
@@ -5,11 +6,14 @@
 </head>
 
 <body>
-<h2>Company Home Page</h2>
-<hr>
+    <h2>Company Home Page</h2>
+    <hr>
 
-Welcome to the company home page!
+    <p>Welcome to the company home page!</p>
 
+    <form:form action="${pageContext.request.contextPath}/logout" method="POST">
+        <input type="submit" value="Logout"/>
+    </form:form>
 </body>
 
 </html>
